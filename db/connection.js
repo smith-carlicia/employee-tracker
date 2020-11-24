1e96ca1b0ea7807150d155460b0925045102901f
Carlicia Smith <smith.carlicia@gmail.com>
2020-11-24 10:47:52 -0500

    created connection.js file into db directory

---
+++ db/connection.js
@@ -0,0 +1,26 @@
+const util = require("util");
+const mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "nschs0912",
+    database: "employeetracker_db"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    questions();
+  });
+
+  connection.query = util.promisify(connection.query);
+
+  module.exports = connection;
--- db/seed.sql
+++ db/seed.sql
@@ -3,8 +3,8 @@ USE employeetracker_db;
 INSERT INTO department(name)
 VALUES ('sales'), ('engineering'), ('finance'), ('legal');
 
-INSERT INTO role (title, salary, depertment, id)
-VALUES ('Sales Lead'), ('Salesperson'), ('Lead Engineer'), ('Software Engineer'), ('Account Manager'), ('Accountant'), ('Legal Team Lead');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 70000, 1), ('Salesperson', 50000, 1), ('Lead Engineer', 80000, 2), ('Software Engineer', 90000, 2), ('Account Manager', 60000, 3), ('Accountant', 40000, 3), ('Legal Team Lead', 80000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Carlicia', 'Smith', 12, NULL), ('Carly', 'Smith', 16, NULL), ('Madeline', 'Smith', 06, NULL), ('Maddie', 'Smith', 20, 08);
+VALUES ('Carlicia', 'Smith', 1, NULL), ('Carly', 'Smith', 2, NULL), ('Madeline', 'Smith', 3, NULL), ('Maddie', 'Smith', 4, NULL);
